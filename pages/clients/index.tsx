3a2c1f09d7219d39c477f1f4110aac2a0c1073a0
Luiz Felipe <luiz.felipe@beeteller.com>
2022-07-08 14:55:00 -0300

    structure

--- pages/clients/index.tsx
+++ pages/clients/index.tsx
@@ -3,28 +3,28 @@ import React from 'react'
 const Clients = () => {
   return (
     <div className='w-full flex flex-col py-16 sm:px-52 justify-center items-center'>
-        <div className='bg-white p-1 rounded shadow-lg'>
-            <table className='table-auto border-collapse border border-slate-400 text-left'>
+        <div className='bg-white rounded shadow-lg'>
+            <table className='table-auto text-left'>
                 <thead>
-                    <tr>
-                        <th className='border border-slate-300 px-6 py-4'>Documento</th>
-                        <th className='border border-slate-300 px-6 py-4'>Nome</th>
+                    <tr className='bg-gray-300 shadow-bottom-inset'>
+                        <th className=' px-6 py-4'>Documento</th>
+                        <th className=' px-6 py-4'>Nome</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className='border border-slate-300 px-6 py-4'>000.000.000-00</td>
-                        <td className='border border-slate-300 px-6 py-4'>$100</td>
+                        <td className=' px-6 py-4'>000.000.000-00</td>
+                        <td className=' px-6 py-4'>$100</td>
                     </tr>
                     <tr>
-                        <td className='border border-slate-300 px-6 py-4'>February</td>
-                        <td className='border border-slate-300 px-6 py-4'>$80</td>
+                        <td className=' px-6 py-4'>February</td>
+                        <td className=' px-6 py-4'>$80</td>
                     </tr>
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td className='border border-slate-300 px-6 py-4'>Sum</td>
-                        <td className='border border-slate-300 px-6 py-4'>$180</td>
+                        <td className=' px-6 py-4'>Sum</td>
+                        <td className=' px-6 py-4'>$180</td>
                     </tr>
                 </tfoot>
             </table>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'bottom-inset': '0 4px 2px -2px #ABABAB' 
+      }
+    },
   },
   plugins: [],
 }
